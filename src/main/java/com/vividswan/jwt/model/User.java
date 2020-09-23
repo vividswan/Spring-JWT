b2d339261f72230d70f70127c0b7f073aa2293ca
Soohwan Park <vividswan@naver.com>
2020-09-24 00:02:54 +0900

    Update User.java

--- src/main/java/com/vividswan/jwt/model/User.java
+++ src/main/java/com/vividswan/jwt/model/User.java
@@ -16,7 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
     private String username;
     private String password;
     private String roles;
