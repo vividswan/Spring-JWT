05317a5e6aa86984c1a6a13bbde3b9f2973aa18b
Soohwan Park <vividswan@naver.com>
2020-10-30 00:50:46 +0900

    Create UserRepository.java

---
+++ src/main/java/com/vividswan/jwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.vividswan.jwt.repository;
+
+import com.vividswan.jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByUsername(String username);
+}
